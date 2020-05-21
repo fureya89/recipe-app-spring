f4ea99a0c1794ab8908f55c6da38113627917872
Ewelina Z <narcyza1989@o2.pl>
2020-05-21 21:31:53 +0200

    Adde Spring Data JPA Repositories (for Recipe, Category and Unit Of Measure entities).  Closes #7

---
+++ src/main/java/com/example/recipeapp/repositories/UnitOfMeasureRepository.java
@@ -0,0 +1,8 @@
+package com.example.recipeapp.repositories;
+
+import com.example.recipeapp.domain.UnitOfMeasure;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+
+}
